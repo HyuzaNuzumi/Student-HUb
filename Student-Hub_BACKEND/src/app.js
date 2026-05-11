dd212b3f0dfb37871627cd196ba9ee84baaa4fcb
HyuzaNuzumi <linglegendstwo@gmail.com>
2026-05-11 11:30:06 +0800

    feat(router): add route untuk profile pada baris ke 13

--- Student-Hub_BACKEND/src/app.js
+++ Student-Hub_BACKEND/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const authRoutes = require('./routes/authRoutes');
+const profileRoutes = require('./routes/profileRoutes');
 const app = express();
 
 //middleware untuk parsing JSON
@@ -8,6 +9,8 @@ app.use(express.json());
 //gunakan route untuk auth
 app.use('/api/auth', authRoutes);
 
-//register 
+//route untuj profile
+app.use('/api/profile', profileRoutes);
+
 
 module.exports = app;
