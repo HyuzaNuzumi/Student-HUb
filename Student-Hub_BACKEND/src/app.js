6db0d5dbd2543c25f4cf8bd51b4505c65cbe28bb
HyuzaNuzumi <linglegendstwo@gmail.com>
2026-07-21 22:39:57 +0800

    style: commend in

--- Student-Hub_BACKEND/src/app.js
+++ Student-Hub_BACKEND/src/app.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const authRoutes = require('./routes/authRoutes');
-const profileRoutes = require('./routes/profileRoutes');
-const app = express();
+// const express = require('express');
+// const authRoutes = require('./routes/authRoutes');
+// const profileRoutes = require('./routes/profileRoutes');
+// const app = express();
 
-//middleware untuk parsing JSON
-app.use(express.json());
+// //middleware untuk parsing JSON
+// app.use(express.json());
 
-//gunakan route untuk auth
-app.use('/api/auth', authRoutes);
+// //gunakan route untuk auth
+// app.use('/api/auth', authRoutes);
 
-//route untuk profile
-app.use('/api/profile', profileRoutes);
+// //route untuk profile
+// // app.use('/api/profile', profileRoutes);
 
 
-module.exports = app;
+// module.exports = app;
